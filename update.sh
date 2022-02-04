2ee936e092b65366d6ad7a57e33d69b3129c9ec8
Calvin Alkan <calvin@snicco.de>
2022-02-05 00:35:19 +0100

    update.sh: clearer output messages

--- update.sh
+++ update.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 our_current_version=$(curl https://api.github.com/repos/sniccowp/php-scoper-wordpress-excludes/tags -s | jq -r .[].name | grep '^[0-9]\.[0-9]*\.[0-9]*$' | sort -nr | head -n1)
-echo "Our Current Version: $our_current_version"
+echo "Current version: $our_current_version"
 
 tmp_their_current_version=$(curl https://api.github.com/repos/php-stubs/wordpress-stubs/tags -s | jq -r .[].name | grep '^v[0-9]\.[0-9]*\.[0-9]*$' | sort -nr | head -n1)
-their_current_version=$(echo $tmp_their_current_version | cut -c 2-)
-echo "Their Current Version: $their_current_version"
+their_current_version=$(echo "$tmp_their_current_version" | cut -c 2-)
+echo "Latest WordPress version: $their_current_version"
 
-if $(dpkg --compare-versions $their_current_version gt $our_current_version);
+if dpkg --compare-versions "$their_current_version" gt "$our_current_version";
 then
         do_update=true
 else
         do_update=false
 fi
 
-if $($do_update);
+if $do_update;
 then
-        composer require php-stubs/wordpress-stubs:$their_current_version --dev
+        composer require php-stubs/wordpress-stubs:"$their_current_version" --dev
         composer update php-stubs/wordpress-stubs
         composer install
         vendor/bin/generate-excludes --json
         git add -A
-        git commit -a -m "Update to: $their_current_version"
+        git commit -a -m "Automatic release for WordPress: $their_current_version"
         git push
-        git tag $their_current_version HEAD
+        git tag "$their_current_version" HEAD
         git push --tags
         exit 0
 else
-	echo "Nothing to do! Everything is up-to-date!"
+	echo "Everything is up-to-date!"
         exit 1
 fi
