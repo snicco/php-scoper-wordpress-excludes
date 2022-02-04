cbb442bfbbc180f0f29d3764bce962dc42ac9c52
Calvin Alkan <calvin@snicco.de>
2022-02-05 00:52:26 +0100

    update.sh: renamed vars and add success message

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-our_current_version=$(curl https://api.github.com/repos/sniccowp/php-scoper-wordpress-excludes/tags -s | jq -r .[].name | grep '^[0-9]\.[0-9]*\.[0-9]*$' | sort -nr | head -n1)
-echo "Current version: $our_current_version"
+current_version=$(curl https://api.github.com/repos/sniccowp/php-scoper-wordpress-excludes/tags -s | jq -r .[].name | grep '^[0-9]\.[0-9]*\.[0-9]*$' | sort -nr | head -n1)
+echo "Current version: $current_version"
 
-tmp_their_current_version=$(curl https://api.github.com/repos/php-stubs/wordpress-stubs/tags -s | jq -r .[].name | grep '^v[0-9]\.[0-9]*\.[0-9]*$' | sort -nr | head -n1)
-their_current_version=$(echo "$tmp_their_current_version" | cut -c 2-)
-echo "Latest WordPress version: $their_current_version"
+tmp_version=$(curl https://api.github.com/repos/php-stubs/wordpress-stubs/tags -s | jq -r .[].name | grep '^v[0-9]\.[0-9]*\.[0-9]*$' | sort -nr | head -n1)
+lastest_version=$(echo "$tmp_version" | cut -c 2-)
+echo "Latest WordPress version: $lastest_version"
 
-if dpkg --compare-versions "$their_current_version" gt "$our_current_version";
+if dpkg --compare-versions "$lastest_version" gt "$current_version";
 then
         do_update=true
 else
@@ -16,15 +16,16 @@ fi
 
 if $do_update;
 then
-        composer require php-stubs/wordpress-stubs:"$their_current_version" --dev
+        composer require php-stubs/wordpress-stubs:"$lastest_version" --dev
         composer update php-stubs/wordpress-stubs
         composer install
         vendor/bin/generate-excludes --json
         git add -A
-        git commit -a -m "Automatic release for WordPress: $their_current_version"
+        git commit -a -m "Automatic release for WordPress: $lastest_version"
         git push
-        git tag "$their_current_version" HEAD
+        git tag "$lastest_version" HEAD
         git push --tags
+        echo "Tagged new version:" "$lastest_version"
         exit 0
 else
 	echo "Everything is up-to-date!"
